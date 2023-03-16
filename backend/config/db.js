136fd9c0900fe8094781b7a3457793b5d9eaf2f9
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-03-16 12:20:55 +0100

    db

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         mongoose.set('strictQuery', false);
-        mongoose.connect()
-    } catch (error) {
-        
+        mongoose.connect(process.env.MONGO_URI, () => console.log("Mongo connecté"));
+    } catch (err) {
+        console.log(err);
+        process.exit();
     }
-}
+};
+
+module.exports = connectDB;
