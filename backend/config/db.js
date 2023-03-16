5ab600159735d80c4580f03235277764d90dc9f2
MathildeCornelis <mathilde.cornelis26@gmail.com>
2023-03-16 12:04:10 +0100

    db

---
+++ backend/config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        mongoose.set('strictQuery', false);
+        mongoose.connect()
+    } catch (error) {
+        
+    }
+}
